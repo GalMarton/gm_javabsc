dc3f4b575304e639d6101014f18da94fd64fb9cb
Marci <gal.marton20@upcmail.hu>
2020-09-12 17:20:46 +0200

    First commit

---
+++ HTMLGenerate/src/main/java/hu/training360/projects/htmlgenerate/Main.java
@@ -0,0 +1,9 @@
+package hu.training360.projects.htmlgenerate;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
